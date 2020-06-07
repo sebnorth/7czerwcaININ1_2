9f214472205d6534e1e88b6c68be3096146b73b9
sebnorth <sebnorth@gmail.com>
2020-06-07 13:04:55 +0200

    kolejne zadania

---
+++ Zad2_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad2_1
+{
+    class Program
+    {
+        static double Stozek(double r, double h) {
+            return Math.PI*r*r*h/3;
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Stozek(1,3));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Zad2_2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad2_2
+{
+    class Program
+    {
+        static double g(uint n) {
+            if (n == 0) return -2;
+            else
+                return (g(n-1) + 3) / 2;
+            
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(g(1));
+            Console.ReadKey();
+        }
+    }
+}
